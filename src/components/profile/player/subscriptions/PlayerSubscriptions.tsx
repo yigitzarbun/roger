b26135bea984c448700b01e9fb63f986a0e8237c
Yigit Zarbun <yzarbun@gmail.com>
2023-09-03 15:34:10 +0300

    add player profile favourites

--- src/components/profile/player/subscriptions/PlayerSubscriptions.tsx
+++ src/components/profile/player/subscriptions/PlayerSubscriptions.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { AiFillStar } from "react-icons/ai";
+
+import { Link } from "react-router-dom";
+
+import paths from "../../../../routing/Paths";
 
 import styles from "./styles.module.scss";
 
@@ -6,29 +12,76 @@ import PageLoading from "../../../../components/loading/PageLoading";
 
 import { useAppSelector } from "../../../../store/hooks";
 
-import { useGetFavouritesQuery } from "../../../../api/endpoints/FavouritesApi";
+import {
+  useGetFavouritesQuery,
+  useUpdateFavouriteMutation,
+} from "../../../../api/endpoints/FavouritesApi";
 import { useGetClubSubscriptionsQuery } from "../../../../api/endpoints/ClubSubscriptionsApi";
 import { useGetStudentGroupsQuery } from "../../../../api/endpoints/StudentGroupsApi";
+import { useGetClubSubscriptionPackagesQuery } from "../../../../api/endpoints/ClubSubscriptionPackagesApi";
+import { useGetClubsQuery } from "../../../../api/endpoints/ClubsApi";
+import { useGetClubSubscriptionTypesQuery } from "../../../../api/endpoints/ClubSubscriptionTypesApi";
+import { useGetUsersQuery } from "../../../../store/auth/apiSlice";
+import { useGetPlayersQuery } from "../../../../api/endpoints/PlayersApi";
+import { useGetTrainersQuery } from "../../../../api/endpoints/TrainersApi";
 
 const PlayerSubscriptions = () => {
   const user = useAppSelector((store) => store.user?.user?.user);
 
-  const { data: favourites, isLoading: isFavouritesLoading } =
-    useGetFavouritesQuery({});
+  const {
+    data: favourites,
+    isLoading: isFavouritesLoading,
+    refetch: refetchFavourites,
+  } = useGetFavouritesQuery({});
 
   const { data: clubSubscriptions, isLoading: isClubSubscriptionsLoading } =
     useGetClubSubscriptionsQuery({});
+
+  const {
+    data: clubSubscriptionPackages,
+    isLoading: isClubSubscriptionPackagesLoading,
+  } = useGetClubSubscriptionPackagesQuery({});
+
+  const {
+    data: clubSubscriptionTypes,
+    isLoading: isClubSubscriptionTypesLoading,
+  } = useGetClubSubscriptionTypesQuery({});
+
+  const { data: clubs, isLoading: isClubsLoading } = useGetClubsQuery({});
+
+  const { data: players, isLoading: isPlayersLoading } = useGetPlayersQuery({});
+
+  const { data: users, isLoading: isUsersLoading } = useGetUsersQuery({});
+
+  const { data: trainers, isLoading: isTrainersLoading } = useGetTrainersQuery(
+    {}
+  );
 
   const { data: studentGroups, isLoading: isStudentGroupsLoading } =
     useGetStudentGroupsQuery({});
 
   const myFavourites = favourites?.filter(
-    (favourite) => favourite.favouriter_id === user?.user_id
+    (favourite) =>
+      favourite.favouriter_id === user?.user_id && favourite.is_active === true
   );
 
   const mySubscriptions = clubSubscriptions?.filter(
     (subscription) => subscription.player_id === user?.user_id
   );
+
+  const [updateFavourite, { isSuccess: isUpdateFavouriteSuccess }] =
+    useUpdateFavouriteMutation({});
+
+  const handleUpdateFavourite = (favouritee_id: number) => {
+    const selectedFavouritee = myFavourites?.find(
+      (favourite) => favourite.favouritee_id === favouritee_id
+    );
+    const updatedFavouriteeData = {
+      ...selectedFavouritee,
+      is_active: false,
+    };
+    updateFavourite(updatedFavouriteeData);
+  };
 
   const myGroups = studentGroups?.filter(
     (group) =>
@@ -39,31 +92,234 @@ const PlayerSubscriptions = () => {
         group.fourth_student_id === user?.user_id)
   );
 
+  const [display, setDisplay] = useState("subscriptions");
+
+  const handleDisplay = (value: string) => {
+    setDisplay(value);
+  };
+
+  useEffect(() => {
+    if (isUpdateFavouriteSuccess) {
+      refetchFavourites();
+    }
+  }, [isUpdateFavouriteSuccess]);
+
   if (
     isFavouritesLoading ||
     isClubSubscriptionsLoading ||
-    isStudentGroupsLoading
+    isStudentGroupsLoading ||
+    isClubSubscriptionPackagesLoading ||
+    isClubsLoading ||
+    isClubSubscriptionTypesLoading ||
+    isUsersLoading ||
+    isPlayersLoading
   ) {
     return <PageLoading />;
   }
   return (
     <div className={styles["player-subscriptions-container"]}>
       <h2>Üyelikler ve Favoriler</h2>
-      <div className={styles["stats-container"]}>
-        <div className={styles.stat}>
-          <p className={styles["stat-number"]}>{mySubscriptions?.length}</p>
-          <p>Üyelik</p>
-        </div>
-        <div className={styles.stat}>
-          <p className={styles["stat-number"]}>{myGroups?.length}</p>
-          <p>Grup</p>
-        </div>
-        <div className={styles.stat}>
-          <p className={styles["stat-number"]}>{myFavourites?.length}</p>
-          <p>Favori</p>
-        </div>
+      <div className={styles["nav-container"]}>
+        <button
+          onClick={() => handleDisplay("subscriptions")}
+          className={
+            display === "subscriptions"
+              ? styles["active-button"]
+              : styles["inactive-button"]
+          }
+        >
+          Üyelikler
+        </button>
+        <button
+          onClick={() => handleDisplay("favourites")}
+          className={
+            display === "favourites"
+              ? styles["active-button"]
+              : styles["inactive-button"]
+          }
+        >
+          Favoriler
+        </button>
+        <button
+          onClick={() => handleDisplay("groups")}
+          className={
+            display === "groups"
+              ? styles["active-button"]
+              : styles["inactive-button"]
+          }
+        >
+          Gruplar
+        </button>
       </div>
-      <button className={styles["view-all-button"]}>Görüntüle</button>
+      {display === "subscriptions" &&
+        (mySubscriptions?.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Kulüp</th>
+                <th>Tür</th>
+                <th>Başlangıç</th>
+                <th>Bitiş</th>
+                <th>Durum</th>
+              </tr>
+            </thead>
+            <tbody>
+              {mySubscriptions?.map((subscription) => (
+                <tr key={subscription.club_subscription_id}>
+                  <td>
+                    {
+                      clubs?.find(
+                        (club) => club.user_id === subscription.club_id
+                      )?.club_name
+                    }
+                  </td>
+                  <td>
+                    {
+                      clubSubscriptionTypes?.find(
+                        (type) =>
+                          type.club_subscription_type_id ===
+                          clubSubscriptionPackages?.find(
+                            (subscriptionPackage) =>
+                              subscriptionPackage.club_subscription_package_id ===
+                              subscription.club_subscription_package_id
+                          )?.club_subscription_type_id
+                      )?.club_subscription_type_name
+                    }
+                  </td>
+                  <td>{subscription.start_date.slice(0, 10)}</td>
+                  <td>{subscription.end_date.slice(0, 10)}</td>
+                  <td>{subscription.is_active ? "Aktif" : "Sonlandı"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Henüz kulüp üyeliği bulunmamaktadır</p>
+        ))}
+      {display === "favourites" &&
+        (myFavourites?.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>İsim</th>
+                <th>Tür</th>
+                <th>Durum</th>
+              </tr>
+            </thead>
+            <tbody>
+              {myFavourites
+                ?.slice(myFavourites.length - 2)
+                ?.map((favourite) => (
+                  <tr key={favourite.favourite_id}>
+                    <td>
+                      <Link
+                        to={`${paths.EXPLORE_PROFILE}${
+                          users?.find(
+                            (user) => user.user_id === favourite.favouritee_id
+                          )?.user_type_id
+                        }/${favourite.favouritee_id}`}
+                        className={styles["view-icon"]}
+                      >
+                        <img
+                          src={
+                            users?.find(
+                              (user) => user.user_id === favourite.favouritee_id
+                            )?.user_type_id === 1
+                              ? players?.find(
+                                  (player) =>
+                                    player.user_id === favourite.favouritee_id
+                                )?.image
+                              : users?.find(
+                                  (user) =>
+                                    user.user_id === favourite.favouritee_id
+                                )?.user_type_id === 2
+                              ? trainers?.find(
+                                  (trainer) =>
+                                    trainer.user_id === favourite.favouritee_id
+                                )?.image
+                              : users?.find(
+                                  (user) =>
+                                    user.user_id === favourite.favouritee_id
+                                )?.user_type_id === 3
+                              ? clubs?.find(
+                                  (club) =>
+                                    club.user_id === favourite.favouritee_id
+                                )?.image
+                              : ""
+                          }
+                          className={styles["favourite-image"]}
+                        />
+                      </Link>
+                    </td>
+                    <td>
+                      {users?.find(
+                        (user) => user.user_id === favourite.favouritee_id
+                      )?.user_type_id === 1
+                        ? `${
+                            players?.find(
+                              (player) =>
+                                player.user_id === favourite.favouritee_id
+                            )?.fname
+                          } ${
+                            players?.find(
+                              (player) =>
+                                player.user_id === favourite.favouritee_id
+                            )?.lname
+                          }`
+                        : users?.find(
+                            (user) => user.user_id === favourite.favouritee_id
+                          )?.user_type_id === 2
+                        ? `${
+                            trainers?.find(
+                              (trainer) =>
+                                trainer.user_id === favourite.favouritee_id
+                            )?.fname
+                          } ${
+                            trainers?.find(
+                              (trainer) =>
+                                trainer.user_id === favourite.favouritee_id
+                            )?.lname
+                          }`
+                        : users?.find(
+                            (user) => user.user_id === favourite.favouritee_id
+                          )?.user_type_id === 3
+                        ? clubs?.find(
+                            (club) => club.user_id === favourite.favouritee_id
+                          )?.club_name
+                        : ""}
+                    </td>
+                    <td>
+                      {users?.find(
+                        (user) => user.user_id === favourite.favouritee_id
+                      )?.user_type_id === 1
+                        ? "Oyuncu"
+                        : users?.find(
+                            (user) => user.user_id === favourite.favouritee_id
+                          )?.user_type_id === 2
+                        ? "Eğitmen"
+                        : users?.find(
+                            (user) => user.user_id === favourite.favouritee_id
+                          )?.user_type_id === 3
+                        ? "Kulüp"
+                        : "Diğer"}
+                    </td>
+                    <td>
+                      <AiFillStar
+                        onClick={() =>
+                          handleUpdateFavourite(favourite.favouritee_id)
+                        }
+                        className={styles["remove-fav-icon"]}
+                      />
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Favorilere eklenen oyuncu, eğitmen veya kulüp bulunmamaktadır</p>
+        ))}
+      <button className={styles["view-all-button"]}>Tümünü Görüntüle</button>
     </div>
   );
 };
